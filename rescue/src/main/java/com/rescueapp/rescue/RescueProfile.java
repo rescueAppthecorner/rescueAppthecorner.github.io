a8dff12a5efb3fb51774a57a37a028679fb8aeb1
Marcel95-sys <farremarcel95@gmail.com>
2022-11-08 15:31:19 +0100

    1

--- rescue/src/main/java/com/rescueapp/rescue/RescueProfile.java
+++ rescue/src/main/java/com/rescueapp/rescue/RescueProfile.java
@@ -18,6 +18,8 @@ public class RescueProfile {
     String email;
     String numTel;
 
+    
+
     public RescueProfile() {}
     
     public RescueProfile(String name, String surname1, String surname2, String email, String numTel) {
@@ -29,19 +31,53 @@ public class RescueProfile {
         
     }
 
-    public int getId() {
-        return id;
-    }
+    //Setters
 
     public void setId(int id) {
         this.id = id;
     }
-    
-    public String getName() {
-        return name;
-    }
+        
     public void setName(String name) {
         this.name = name;
+    }
+    public void setSurname1(String surname1) {
+        this.surname1 = surname1;
+    }
+
+    public void setSurname2(String surname2) {
+        this.surname2 = surname2;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
+    }
+
+    //Getters
+    
+    public int getId() {
+        return id;
+    }
+public String getName() {
+        return name;
+    }
+    public String getSurname1() {
+        return surname1;
+    }
+
+    public String getSurname2() {
+        return surname2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNumTel() {
+        return numTel;
     }
     
     
