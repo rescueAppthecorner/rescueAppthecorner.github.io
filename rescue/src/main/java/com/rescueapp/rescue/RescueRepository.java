40ae2c78fe7cc74dc084dfe44e25c792fe11932e
Marcel95-sys <farremarcel95@gmail.com>
2022-11-09 12:58:25 +0100

    11

--- rescue/src/main/java/com/rescueapp/rescue/RescueRepository.java
+++ rescue/src/main/java/com/rescueapp/rescue/RescueRepository.java
@@ -1,10 +1,11 @@
 package com.rescueapp.rescue;
 
-import java.util.List;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RescueRepository extends JpaRepository <RescueProfile, Integer> {
 
-    List<RescueProfile> findByEmail(String email);
+    RescueProfile findByEmail(String email);
     
 }
