9658eac89ce474726ee3240c2f5c3becc8e13e0d
Marcel95-sys <farremarcel95@gmail.com>
2022-11-09 09:36:22 +0100

    1

--- rescue/src/main/java/com/rescueapp/rescue/RescueConfig.java
+++ rescue/src/main/java/com/rescueapp/rescue/RescueConfig.java
@@ -19,8 +19,6 @@ public class RescueConfig {
     String email;
     String numTel;
 
-    
-
     public RescueConfig() {}
     
     public RescueConfig(String passw, String name, String surname1, String surname2, String email, String numTel) {
@@ -35,61 +33,13 @@ public class RescueConfig {
 
     //Setters
 
-
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setPassw(String passw) {
-        this.passw = passw;
-    }
-        
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setSurname1(String surname1) {
-        this.surname1 = surname1;
-    }
-
-    public void setSurname2(String surname2) {
-        this.surname2 = surname2;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setNumTel(String numTel) {
-        this.numTel = numTel;
-    }
-
     //Getters
-
-    public String getPassw() {
-        return passw;
-    }
 
     public int getId() {
         return id;
     }
-    public String getName() {
-        return name;
-    }
-    public String getSurname1() {
-        return surname1;
-    }
-
-    public String getSurname2() {
-        return surname2;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getNumTel() {
-        return numTel;
-    }
-    
-    
 }
