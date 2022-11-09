c7cf86b22954edb8b7aa02383be4bdd507b0159f
Marcel95-sys <farremarcel95@gmail.com>
2022-11-09 09:48:31 +0100

    1

--- rescue/src/main/java/com/rescueapp/rescue/RescueConfig.java
+++ rescue/src/main/java/com/rescueapp/rescue/RescueConfig.java
@@ -11,7 +11,8 @@ public class RescueConfig {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-    int id;
+    int idCampo;
+    int idUsuario;
     String passw; 
     String name;
     String surname1;
@@ -33,13 +34,70 @@ public class RescueConfig {
 
     //Setters
 
-    public void setId(int id) {
-        this.id = id;
+    public void setIdCampo(int idCampo) {
+        this.idCampo = idCampo;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public void setPassw(String passw) {
+        this.passw = passw;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname1(String surname1) {
+        this.surname1 = surname1;
+    }
+
+    public void setSurname2(String surname2) {
+        this.surname2 = surname2;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNumTel(String numTel) {
+        this.numTel = numTel;
     }
 
     //Getters
 
-    public int getId() {
-        return id;
+    public int getIdCampo() {
+        return idCampo;
     }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getPassw() {
+        return passw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname1() {
+        return surname1;
+    }
+
+    public String getSurname2() {
+        return surname2;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNumTel() {
+        return numTel;
+    }
+    
 }
