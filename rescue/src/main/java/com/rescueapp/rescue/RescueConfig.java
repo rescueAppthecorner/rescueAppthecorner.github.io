47296e52d3f14bad075229a469a2a39fa13d1def
Marcel95-sys <farremarcel95@gmail.com>
2022-11-09 10:07:13 +0100

    aaaaaaaaaaaaaaaaaaaa

--- rescue/src/main/java/com/rescueapp/rescue/RescueConfig.java
+++ rescue/src/main/java/com/rescueapp/rescue/RescueConfig.java
@@ -10,64 +10,37 @@ import javax.persistence.Id;
 public class RescueConfig {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     int idCampo;
+
     int idUsuario;
-    String passw; 
-    String name;
-    String surname1;
-    String surname2;
-    String email;
-    String numTel;
+    String nombreParam; 
+    String contenido;
 
     public RescueConfig() {}
     
-    public RescueConfig(String passw, String name, String surname1, String surname2, String email, String numTel) {
-        this.passw = passw;
-        this.name = name;
-        this.surname1 = surname1;
-        this.surname2 = surname2;
-        this.email = email;
-        this.numTel = numTel;
+    public RescueConfig(int idUsuario, String nombreParam, String contenido) {
+        this.idUsuario = idUsuario;
+        this.nombreParam = nombreParam;
+        this.contenido = contenido;
         
     }
 
     //Setters
 
-    public void setIdCampo(int idCampo) {
-        this.idCampo = idCampo;
-    }
-
     public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
 
-    public void setPassw(String passw) {
-        this.passw = passw;
+    public void setNombreParam(String nombreParam) {
+        this.nombreParam = nombreParam;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname1(String surname1) {
-        this.surname1 = surname1;
-    }
-
-    public void setSurname2(String surname2) {
-        this.surname2 = surname2;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setNumTel(String numTel) {
-        this.numTel = numTel;
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
     }
 
     //Getters
-
+    
     public int getIdCampo() {
         return idCampo;
     }
@@ -76,28 +49,12 @@ public class RescueConfig {
         return idUsuario;
     }
 
-    public String getPassw() {
-        return passw;
+    public String getNombreParam() {
+        return nombreParam;
     }
 
-    public String getName() {
-        return name;
+    public String getContenido() {
+        return contenido;
     }
 
-    public String getSurname1() {
-        return surname1;
-    }
-
-    public String getSurname2() {
-        return surname2;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getNumTel() {
-        return numTel;
-    }
-    
 }
